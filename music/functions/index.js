71080d3d32d8ad0ea441953938b25dd2d3082bb6
xoak <54974852+xoak@users.noreply.github.com>
2020-04-08 21:57:15 -0700

    Remove test file

--- music/functions/index.js
+++ music/functions/index.js
@@ -2,6 +2,5 @@ module.exports = {
     play: require('./play'),
     playSong: require('./playSong'),
     queue: require('./queue'),
-    replay: require('./replay'),
-    test: require('./test')
+    replay: require('./replay')
 };
