f75953b43e7bd8eab02a949a75ba7c7e6f921707
xoak <54974852+xoak@users.noreply.github.com>
2020-05-02 16:35:14 -0700

    chore: update scrape-youtube to 0.1.7

--- music/functions/search.js
+++ music/functions/search.js
@@ -1,25 +1,19 @@
-const ytscrape = require('scrape-youtube');
+const ytscrape = require('scrape-youtube').youtube;
 
 module.exports = function search(message) {
     let query = message.content.replace(/^[\S]+[\s]+/, '');
     console.log(query);
-    ytscrape.search(query, {
-        limit : 1,
+    ytscrape.searchOne(query, {
         type : 'video'
     }).then(function(results){
         console.log(results);
-        let videoID = results[0].link.slice(28,39);
-        let title = results[0].title;
-        if (videoID === 'ww.googlead') {
-            message.channel.send('try something else: ' + '`' + query + '`');
-            message.channel.send('```' + results[0].link + '```');
-        } else {
-            console.log(videoID);
-            message.client.music.queue.add(videoID, message);
-            message.channel.send('Adding `' + title + '` to the queue.');
-            console.log(message.client.songQueue);
-            if (!message.client.playing) message.client.music.next(message);
-        }
+        let videoID = results.id;
+        let title = results.title;
+        console.log(videoID);
+        message.client.music.queue.add(videoID, message);
+        message.channel.send('Adding `' + title + '` to the queue.');
+        console.log(message.client.songQueue);
+        if (!message.client.playing) message.client.music.next(message);
     }, function(err){
         console.log(err);
     });
